455501d3785771f58a98658d72baa788a258c8b6
Daniel Straneus <danielstraneus@gmail.com>
2022-04-22 09:06:22 +0200

    --

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-// Importera npm-paket och moduler
-// Allmänna inställningar
 import express from "express";
 import cors from "cors";
 import path from "path";
@@ -12,24 +10,23 @@ import hamsters from "./routes/hamsters.js";
 const staticFolder = path.join(__dirname, "public");
 
 // Middleware
-// CORS öppnar vårt projekt så det kan användas från andra domäner
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Logger - skriv ut information om inkommande request
+// Logger
 app.use((req, res, next) => {
   console.log(`Logger: ${req.method}  ${req.url} `, req.body);
   next();
 });
 
-// Serve static files in this folder
+// Static
 app.use(express.static(staticFolder));
 
 // Routes
 app.use("/hamsters", hamsters);
 
-// Starta server
+// Server
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}.`);
 });
